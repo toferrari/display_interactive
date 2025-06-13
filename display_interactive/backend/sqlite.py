111c49b0e6d033dcac68d62a4e5ebee62dfe09de
Ferrari <tony.ferrari@iqvia.com>
2025-06-13 17:05:10 +0200

    add log

--- display_interactive/backend/sqlite.py
+++ display_interactive/backend/sqlite.py
@@ -2,7 +2,7 @@ from display_interactive.models.model import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
+DATABASE_URL = "sqlite:///./purchases.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
--- display_interactive/routers/import_csv.py
+++ display_interactive/routers/import_csv.py
@@ -1,9 +1,13 @@
+import logging
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends
 
 from display_interactive.backend.sqlite import get_db
 from display_interactive.schemas.import_csv import ImportCSV
 from display_interactive.services.import_csv import import_csv
+
+log = logging.getLogger(__name__)
 
 router = APIRouter(tags=["csv"], prefix="")
 
@@ -16,5 +20,7 @@ async def import_csv_files(
     """
     Import data from CSV files into the database.
     """
-    await import_csv(request, db)
-
+    try:
+        await import_csv(request, db)
+    except Exception as exc:
+        raise HTTPException(status_code=500, detail=f"An error occurred while importing CSV files: {exc}")
